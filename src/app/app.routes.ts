4aea0b6f6ebc11335d5e8397ecbded15939671f4
Liam Langstaff <liamlangstaff@gmail.com>
2024-05-10 20:26:27 +0100

    Update default route to GenerateComponent
    The default route of the application was updated from the HomepageComponent to the GenerateComponent. The HomepageComponent was also removed from the import statements, as it was no longer needed.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 import { GenerateComponent } from './features/generate/generate.component';
 import { resultGuard } from './features/generate/guards/result.guard';
 import { ResultComponent } from './features/generate/result/result.component';
-import { HomepageComponent } from './features/homepage/homepage.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomepageComponent,
+    component: GenerateComponent,
   },
   {
     path: 'generate',
