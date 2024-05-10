1f32a664ea783cfca86d0339b05b998ad63dbb97
Liam Langstaff <liamlangstaff@gmail.com>
2024-05-10 20:23:23 +0100

    Add Supabase integration and homepage component
    Integrated Supabase into the project, adding Supabase SDK to the project's dependencies. Also, the homepage component has been added along with the necessary routing modifications. Environments for development and production have been created for proper Supabase integration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { GenerateComponent } from './features/generate/generate.component';
 import { resultGuard } from './features/generate/guards/result.guard';
 import { ResultComponent } from './features/generate/result/result.component';
+import { HomepageComponent } from './features/homepage/homepage.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'generate',
-    pathMatch: 'full',
+    component: HomepageComponent,
   },
   {
     path: 'generate',
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  isProduction: false,
+  SUPABASE_URL: 'https://eamejbzsjhqyuakkxrxq.supabase.co',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhbWVqYnpzamhxeXVha2t4cnhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEwMDgzMjksImV4cCI6MjAxNjU4NDMyOX0.4wwX47RO9CUDdKfGzofVi6_7WAf1Km8xVeNV0pi_J-c',
+};
