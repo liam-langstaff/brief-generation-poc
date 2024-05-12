3b36fefd1a5f9c76242cf7b06e7a71b55adcce77
Liam Langstaff <liamlangstaff@gmail.com>
2024-05-12 20:29:36 +0100

    Update styles and add router link to buttons
    Styled several elements such as buttons and dividers, and linked the 'View Brief' buttons to the relevant route. Also updated the 'my-briefs' component to display a random background color for the '.library-card__image' elements upon view initialization.

--- src/app/features/my-briefs/my-briefs.component.ts
+++ src/app/features/my-briefs/my-briefs.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { AsyncPipe, NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-my-briefs',
   standalone: true,
-  imports: [AsyncPipe, NgOptimizedImage],
+  imports: [AsyncPipe, NgOptimizedImage, RouterLink],
   templateUrl: './my-briefs.component.html',
   styleUrl: './my-briefs.component.scss',
 })
-export class MyBriefsComponent {}
+export class MyBriefsComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    document.querySelectorAll('.library-card__image').forEach((item) => {
+      if (item instanceof HTMLElement) {
+        console.log(item);
+        item.style.backgroundColor = this.getRandomColor();
+      }
+    });
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+}
